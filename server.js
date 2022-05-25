f7657eda02010b80395b451cf459594dbb377c6a
endarova <maria.endarova@telerik.com>
2022-05-25 10:31:01 +0300

    test

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.use(express.static("express"));
 // default URL for website
 app.use('/',  function(req,res){
     res.sendFile(path.join(__dirname+'/express/index.html'));
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     //__dirname : It will resolve to your project folder.
   });
 const server = http.createServer(app);
